a622057705270226df4ed366eac6e6008e791378
George Lydakis <lydakis.george@gmail.com>
2016-06-03 21:53:07 +0300

    Minor fixes

--- spark/recommendations/ratings.py
+++ spark/recommendations/ratings.py
@@ -18,29 +18,41 @@ def parse_range(daterange):
     else:
         raise
 
+def append_record(item, record):
+    item[1].update(record)
+    return item
+
 def filter_daterange(rdd, end_date, daterange):
     import datetime as dt
-    end_date = dt.datetime.strptime(end_date, '%Y-%m-%d')
-    start_date = end_date - parse_range(daterange)
+    end = dt.datetime.strptime(end_date, '%Y-%m-%d')
+    start = end - parse_range(daterange)
     return rdd.filter(lambda item: dt.datetime.strptime(
-            item[1]['date_closed'], '%Y-%m-%dT%H:%M:%S.%fZ') < end_date) \
+            item[1]['date_closed'], '%Y-%m-%dT%H:%M:%S.%fZ') < end) \
         .filter(lambda item: dt.datetime.strptime(
-            item[1]['date_closed'], '%Y-%m-%dT%H:%M:%S.%fZ') > start_date)
+            item[1]['date_closed'], '%Y-%m-%dT%H:%M:%S.%fZ') > start) \
+        .map(lambda item: append_record(item, {
+            'end_date': end_date,
+            'range': daterange
+        }))
 
-def count_pairs(provider, rdd, end_date, daterange):
-    rdd = filter_daterange(rdd, end_date, daterange)
+def count_pairs(rdd, provider):
+    doc = rdd.first()[1]
+    end_date = doc['end_date']
+    daterange = doc['range']
     pair_counts = rdd \
         .filter(lambda item: provider == item[1]['provider_id']) \
         .map(lambda item: item[1]['currency_pair']) \
         .map(lambda word: (word, 1)) \
         .reduceByKey(lambda a, b: a+b) \
-        .map(lambda item: ('key', {
-            'provider_id': provider,
-            'end_date': end_date,
-            'range': daterange,
-            'currency_pair': item[0],
-            'count': item[1]
-        }))
+        .map(lambda item: (
+            str(provider) + ':' + item[0] + ':' + end_date + ':' + daterange, {
+                'provider_id': provider,
+                'end_date': end_date,
+                'range': daterange,
+                'currency_pair': item[0],
+                'count': item[1]
+            }
+        )).cache()
     return pair_counts
 
 def calc_rating(rdd):
@@ -48,30 +60,23 @@ def calc_rating(rdd):
         1, key = lambda item: -item[1]['count'])[0][1]['count']
     min_count = rdd.takeOrdered(
         1, key = lambda item: item[1]['count'])[0][1]['count']
-    return rdd.map(lambda item: ('key', {
-        'rating_id': str(item[1]['provider_id']) + ':' + \
-            item[1]['currency_pair'] + ':' + \
-            item[1]['end_date'] + ':' + item[1]['range'],
-        'provider_id': item[1]['provider_id'],
-        'end_date': item[1]['end_date'],
-        'range': item[1]['range'],
-        'currency_pair': item[1]['currency_pair'],
-        'count': item[1]['count'],
-        'rating': 1+(9*(item[1]['count']-min_count)/(max_count-min_count))
+    return rdd.map(lambda item: append_record(item, {
+        'rating_id': item[0],
+        'rating': 1+(9.0*(item[1]['count']-min_count)/(max_count-min_count))
             if max_count != min_count else 10
     }))
 
 def get_ratings(rdd, end_date, daterange):
+    rdd = filter_daterange(rdd, end_date, daterange)
     providers = get_providers(rdd).collect()
-    return [calc_rating(count_pairs(
-        provider, rdd, end_date, daterange)) for provider in providers]
+    return [calc_rating(count_pairs(rdd, provider)) for provider in providers]
 
 def save_ratings(ratings, index, key=None):
     for rating in ratings:
         save_es_rdd(rating, index, key)
 
 if __name__ == '__main__':
-    conf = SparkConf().setAppName('Currency Recommendations')
+    conf = SparkConf().setAppName('Compute Currency Ratings')
     sc = SparkContext(conf=conf)
 
     es_rdd = get_es_rdd('forex/transaction')
