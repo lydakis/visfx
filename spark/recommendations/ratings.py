af8aca1c8e59a08ed31c974a586663e9d535cfd6
George Lydakis <lydakis.george@gmail.com>
2016-06-06 21:51:43 +0300

    Added more features to compute ratings

--- spark/recommendations/ratings.py
+++ spark/recommendations/ratings.py
@@ -37,6 +37,9 @@ def filter_daterange(rdd, end_date, daterange):
             'range': daterange
         }))
 
+def filter_provider(rdd, provider):
+    return rdd.filter(lambda item: provider == item[1]['provider_id'])
+
 def count_pairs(rdd, provider):
     doc = rdd.first()[1]
     end_date = doc['end_date']
@@ -44,8 +47,8 @@ def count_pairs(rdd, provider):
     pair_counts = rdd \
         .filter(lambda item: provider == item[1]['provider_id']) \
         .map(lambda item: item[1]['currency_pair']) \
-        .map(lambda word: (word, 1)) \
-        .reduceByKey(lambda a, b: a+b) \
+        .map(lambda pair: (pair, 1)) \
+        .reduceByKey(lambda a, b: a + b) \
         .map(lambda item: (
             str(provider) + ':' + item[0] + ':' + end_date + ':' + daterange, {
                 'provider_id': provider,
@@ -56,6 +59,76 @@ def count_pairs(rdd, provider):
             }
         ))
     return pair_counts
+
+def count_pairs(rdd):
+    return rdd \
+        .map(lambda item:
+            ((item[1]['provider_id'], item[1]['currency_pair']), 1)) \
+        .reduceByKey(lambda a, b: a + b)
+
+def total_amount(rdd):
+    return rdd \
+        .map(lambda item: ((item[1]['provider_id'], item[1]['currency_pair']),
+            item[1]['amount'])) \
+        .reduceByKey(lambda a, b: a + b)
+
+def total_pair_pnl(rdd):
+    return rdd \
+        .map(lambda item: ((item[1]['provider_id'], item[1]['currency_pair']),
+            item[1]['net_pnl'])) \
+        .reduceByKey(lambda a, b: a + b)
+
+def total_provider_pnl(rdd):
+    return rdd \
+        .map(lambda item: (item[1]['provider_id'], item[1]['net_pnl'])) \
+        .reduceByKey(lambda a, b: a + b)
+
+def pnl_per_amount(pnl_rdd, amount_rdd):
+    per_amount = amount_rdd.map(lambda item: (item[0], 1.0 / item[1]))
+    union = pnl_rdd.union(per_amount)
+    return union.reduceByKey(lambda a, b: a * b)
+
+def normalize_feature(rdd):
+    max_value = rdd.map(lambda item: item[1]).max()
+    count = rdd.count()
+    average = rdd\
+        .map(lambda item: item[1]) \
+        .reduce(lambda a, b: a + b) / float(count)
+    return rdd.map(lambda item: (item[0], float(item[1] - average) / max_value))
+
+def generate_features(rdd):
+    count_rdd = count_pairs(rdd)
+    pnl_rdd = total_pair_pnl(rdd)
+    amount_rdd = total_amount(rdd)
+    pnl_per_amount_rdd = pnl_per_amount(pnl_rdd, amount_rdd)
+    provider_pnl_rdd = total_provider_pnl(rdd)
+    return {
+        'per_currency': {
+            'pair_counts': normalize_feature(count_rdd),
+            'total_amount' normalize_feature(amount_rdd),
+            'total_pair_pnl': normalize_feature(pnl_rdd),
+            'pnl_per_amount': normalize_feature(pnl_per_amount_rdd)
+        },
+        'per_provider': {
+            'total_provider_pnl': normalize_feature(provider_pnl_rdd)
+        }
+    }
+
+def generate_weights():
+    return {
+        'per_currency': {
+            'pair_counts': 0.5,
+            'total_amount' 0.5,
+            'total_pair_pnl': 0.5,
+            'pnl_per_amount': 0.5
+        },
+        'per_provider': {
+            'total_provider_pnl': 0.5
+        }
+    }
+
+def calc_rating(features, weights):
+    pass
 
 def calc_rating(rdd):
     max_count = rdd.takeOrdered(
