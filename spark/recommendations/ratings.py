76e6691284ebfc4c2cf5d80a2627eaea3e9719c2
George Lydakis <lydakis.george@gmail.com>
2016-06-14 21:46:50 +0300

    Added PCA algorithm, plus you can now plug and play-use different rating algorithms

--- spark/recommendations/ratings.py
+++ spark/recommendations/ratings.py
@@ -1,6 +1,8 @@
 from pyspark import SparkContext, SparkConf, StorageLevel
-from elasticsearch_interface import es_read_conf, es_write_conf, \
-    get_es_rdd, save_es_rdd
+import numpy as np
+from numpy.linalg import svd
+from elasticsearch_interface \
+    import get_es_rdd, save_es_rdd, get_currency_pair_dict
 from utils import parse_range, parse_dates, modify_record
 
 def count_pairs(rdd):
@@ -44,68 +46,77 @@ def generate_features(rdd):
     pnl_rdd = total_pair_pnl(rdd)
     amount_rdd = total_amount(rdd)
     pnl_per_amount_rdd = pnl_per_amount(pnl_rdd, amount_rdd)
-    provider_pnl_rdd = total_provider_pnl(rdd)
+    provider_pnl_rdd = canonicalize_keys(
+        total_provider_pnl(rdd), get_keys(pnl_rdd))
     return {
-        'per_currency': {
-            'pair_counts': normalize_feature(count_rdd),
-            'total_amount': normalize_feature(amount_rdd),
-            'total_pair_pnl': normalize_feature(pnl_rdd),
-            'pnl_per_amount': normalize_feature(pnl_per_amount_rdd)
-        },
-        'per_provider': {
-            'total_provider_pnl': normalize_feature(provider_pnl_rdd)
-        }
+        'pair_counts': normalize_feature(count_rdd),
+        'total_amount': normalize_feature(amount_rdd),
+        'total_pair_pnl': normalize_feature(pnl_rdd),
+        'pnl_per_amount': normalize_feature(pnl_per_amount_rdd),
+        'total_provider_pnl': normalize_feature(provider_pnl_rdd)
     }
+
+def get_keys(rdd):
+    return rdd.sortByKey().map(lambda item: (item[0]))
+
+def canonicalize_keys(feature, keys):
+    return feature.join(keys).map(lambda x: ((x[0], x[1][1]), x[1][0]))
 
 def generate_weights():
     return {
-        'per_currency': {
-            'pair_counts': 1.0,
-            'total_amount': 1.0,
-            'total_pair_pnl': 1.0,
-            'pnl_per_amount': 1.0
-        },
-        'per_provider': {
-            'total_provider_pnl': 1.0
-        }
+        'pair_counts': 1.0,
+        'total_amount': 1.0,
+        'total_pair_pnl': 1.0,
+        'pnl_per_amount': 1.0,
+        'total_provider_pnl': 1.0
     }
 
 def apply_weights(features, weights):
-    for feature_type, feature_dict in features.iteritems():
-        for feature, _ in feature_dict.iteritems():
-            features[feature_type][feature] = features[feature_type][feature] \
-                .map(lambda item:
-                    (item[0], weights[feature_type][feature] * item[1])).cache()
+    for feature, _ in features.iteritems():
+        features[feature] \
+            .foreach(lambda item: (item[0], weights[feature] * item[1]))
     return features
 
-def calc_ratings(sc, features):
-    per_currency = sc \
-        .union([feature_value
-            for _,feature_value in features['per_currency'].iteritems()]) \
+def generate_feature_matrix(features):
+    return [feature_value.sortByKey().map(lambda item: item[1]).collect()
+         for _, feature_value in features.iteritems()]
+
+def format_pca(ratings, keys):
+    ratings = ratings.zipWithIndex().map(lambda x: (x[1], x[0]))
+    keys = keys.zipWithIndex().map(lambda x: (x[1], x[0]))
+    return keys.join(ratings) \
+        .map(lambda x: x[1]) \
+        .map(lambda item: (item[0][0], {
+            'provider_id': item[0][0],
+            'currency_pair': item[0][1],
+            'rating': item[1]
+        }))
+
+def pca(sc, features):
+    feature_matrix = generate_feature_matrix(features)
+    m = len(feature_matrix[0])
+    sigma = (1.0 / m) * np.dot(feature_matrix, np.transpose(feature_matrix))
+    U, _, _ = svd(sigma)
+    ratings = sc.parallelize(
+        np.dot(np.transpose(U[1]), feature_matrix).tolist())
+    keys = get_keys(features['pair_counts'])
+    return format_pca(ratings, keys).cache()
+
+def linear_combination(sc, features):
+    weights = generate_weights()
+    weighted_features = apply_weights(features, weights)
+    return sc \
+        .union([feature_values
+            for _,feature_values in features.iteritems()]) \
         .reduceByKey(lambda a, b: a + b) \
         .map(lambda item: (item[0][0], {
             'provider_id': item[0][0],
             'currency_pair': item[0][1],
             'rating': item[1]
         })).cache()
-    per_provider = sc \
-        .union([feature_value
-            for _,feature_value in features['per_provider'].iteritems()]) \
-        .reduceByKey(lambda a, b: a + b) \
-        .map(lambda item: (item[0], {
-            'provider_id': item[0],
-            'rating': item[1]
-        })).cache()
-    def add_ratings(r1, r2):
-        return {
-            'provider_id': r1['provider_id'],
-            'currency_pair': r1['currency_pair'] \
-                if 'currency_pair' in r1 else r2['currency_pair'],
-            'rating': r1['rating'] + r2['rating']
-        }
-    return sc \
-        .union([per_currency, per_provider]) \
-        .reduceByKey(lambda a, b: add_ratings(a, b))
+
+def calc_ratings(sc, features, method):
+    return method(sc, features)
 
 def format_ratings(ratings, end_date, daterange):
     max_rating = ratings.map(lambda item: item[1]['rating']).cache().max()
@@ -124,9 +135,7 @@ def format_ratings(ratings, end_date, daterange):
 
 def get_ratings(sc, rdd, end_date, daterange):
     features = generate_features(rdd)
-    weights = generate_weights()
-    weighted_features = apply_weights(features, weights)
-    ratings = calc_ratings(sc, weighted_features).cache()
+    ratings = calc_ratings(sc, features, method=pca)
     return format_ratings(ratings, end_date, daterange)
 
 def save_ratings(ratings, index, key=None):
@@ -137,7 +146,8 @@ if __name__ == '__main__':
     sc = SparkContext(conf=conf)
 
     start_date, end_date = parse_dates('2015-05-01', '1d')
-    es_rdd = get_es_rdd(sc, 'forex/transaction', 'date_closed',
-        start_date, end_date).persist(StorageLevel.MEMORY_AND_DISK)
+    es_rdd = get_es_rdd(sc, index='forex/transaction', date_field='date_closed',
+        start_date=start_date, end_date=end_date) \
+            .persist(StorageLevel.MEMORY_AND_DISK)
     ratings = get_ratings(sc, es_rdd, '2015-05-01', '1d')
     save_ratings(ratings, 'forex/rating', 'rating_id')
