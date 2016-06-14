77fd968cf2d71ba8d8d82304544da1c510464a15
George Lydakis <lydakis.george@gmail.com>
2016-06-14 21:44:04 +0300

    Started working on ALS recommendation algorithm

--- spark/recommendations/elasticsearch_interface.py
+++ spark/recommendations/elasticsearch_interface.py
@@ -1,10 +1,13 @@
-def es_read_conf(index, date_field=None, start_date=None, end_date=None):
+def es_read_conf(
+    index, query=None, date_field=None, start_date=None, end_date=None):
     conf = {
        'es.nodes': 'search',
        'es.port': '9200',
        'es.resource': index
     }
-    if start_date != None and end_date != None:
+    if query !=None:
+        conf['es.query'] = query
+    elif date_field != None and start_date != None and end_date != None:
         conf['es.query'] = '''{
             "query": {
                 "match_all": {
@@ -32,12 +35,13 @@ def es_write_conf(index, key=None):
         conf['es.mapping.id'] = key
     return conf
 
-def get_es_rdd(sc, index, date_field=None, start_date=None, end_date=None):
+def get_es_rdd(
+    sc, index, query=None, date_field=None, start_date=None, end_date=None):
     return sc.newAPIHadoopRDD(
         inputFormatClass='org.elasticsearch.hadoop.mr.EsInputFormat',
         keyClass='org.apache.hadoop.io.NullWritable',
         valueClass='org.elasticsearch.hadoop.mr.LinkedMapWritable',
-        conf=es_read_conf(index, date_field, start_date, end_date))
+        conf=es_read_conf(index, query, date_field, start_date, end_date))
 
 def save_es_rdd(rdd, index, key=None):
     rdd.saveAsNewAPIHadoopFile(
@@ -46,3 +50,8 @@ def save_es_rdd(rdd, index, key=None):
         keyClass="org.apache.hadoop.io.NullWritable",
         valueClass="org.elasticsearch.hadoop.mr.LinkedMapWritable",
         conf=es_write_conf(index, key))
+
+def get_currency_pair_dict(sc):
+    return dict(get_es_rdd(sc, 'forex/currency_pair') \
+        .map(lambda item:
+            (item[1]['currency_pair'], item[1]['currency_pair_id'])).collect())
--- spark/recommendations/recommendations.py
+++ spark/recommendations/recommendations.py
@@ -1,19 +1,55 @@
-from pyspark.mllib.recommendation import ALS, Rating
-from elasticsearch_interface import es_read_conf, es_write_conf, \
-    get_es_rdd, save_es_rdd
-import hashlib
+from pyspark import SparkContext, SparkConf, StorageLevel
+from pyspark.ml.recommendation import ALS
+from elasticsearch_interface import get_es_rdd, save_es_rdd, \
+    get_currency_pair_dict
+from utils import parse_range, parse_dates
+import math
 
-if __name__ == '__main__':
-    data = get_es_rdd('forex/rating')
-    ratings = data.map(lambda item: Rating(
-        int(item[1]['provider_id']),
-        int(hashlib.sha1(item[1]['currency_pair']).hexdigest(), 16) % (10 ** 8),
-        float(item[1]['rating']))).collect()
-    rank = 10
-    numIterations = 10
-    model = ALS.train(ratings, rank, numIterations)
-    testdata = ratings.map(lambda p: (p[0], p[1]))
-    predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
-    ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
-    MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
-    print("Mean Squared Error = " + str(MSE))
+
+conf = SparkConf().setAppName('Currency Recommendations')
+sc = SparkContext(conf=conf)
+
+start_date, end_date = parse_dates('2015-05-01', '1d')
+currency_pair_dict = get_currency_pair_dict(sc)
+ratings = get_es_rdd(
+    sc, index='forex/rating', date_field='start_date',
+    start_date=start_date, end_date=end_date) \
+        .map(lambda item: (
+            item[1]['provider_id'],
+            currency_pair_dict[item[1]['currency_pair']],
+            item[1]['rating'])).toDF(["user", "item", "rating"])
+
+training_RDD, validation_RDD, test_RDD = \
+    ratings.randomSplit([6.0, 2.0, 2.0], seed=0L)
+validation_for_predict_RDD = validation_RDD.map(lambda x: (x[0], x[1]))
+test_for_predict_RDD = test_RDD.map(lambda x: (x[0], x[1]))
+
+seed = 5L
+iterations = 10
+regularization_parameter = 0.1
+ranks = [4, 8, 12]
+errors = [0, 0, 0]
+err = 0
+tolerance = 0.02
+
+min_error = float('inf')
+best_rank = -1
+best_iteration = -1
+
+for rank in ranks:
+    model = ALS.train(ratings, rank, seed=seed, iterations=iterations,
+        lambda_=regularization_parameter)
+    predictions = model.predictAll(validation_for_predict_RDD).map(
+        lambda r: ((r[0], r[1]), r[2]))
+    rates_and_preds = validation_RDD.map(
+        lambda r: ((r[0], r[1]), r[2])).join(predictions)
+    error = math.sqrt(rates_and_preds.map(
+        lambda r: (r[1][0] - r[1][1])**2).mean())
+    errors[err] = error
+    err += 1
+    print 'For rank %s the RMSE is %s' % (rank, error)
+    if error < min_error:
+        min_error = error
+        best_rank = rank
+
+print 'The best model was trained with rank %s' % best_rank
